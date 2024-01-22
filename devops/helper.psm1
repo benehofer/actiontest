754e8d3b9758587cc899b5ff83bb45682d499c68
hofb@wid.dev <hofb@wid.dev>
2024-01-22 20:01:29 +0100

    sixtyfive

---
+++ devops/helper.psm1
@@ -0,0 +1,122 @@
+function New-Result() {
+    param(
+        [parameter(Mandatory=$true)][bool]$success,
+        [string]$message="",
+        [System.Exception]$exception=$null,
+        [System.Object]$value=$null,
+        [ValidateSet("Verbose","Information","Warning","Error")]$logLevel="Information"
+    )
+    if (($exception -ne $null) -and ($logLevel -eq "Information")) {$logLevel="Error"}
+    [PSCustomObject]@{
+        Success=$success
+        Message=$message
+        Exception=$exception
+        Value=$value
+        LogLevel=$logLevel
+    }
+}
+function Write-dplResult() {
+    param
+    (
+	    [Parameter(Mandatory = $true, ValueFromPipeline = $true)][PSCustomObject[]]$result,
+        [switch]$NoPassThru
+    )
+    BEGIN {}
+    PROCESS {
+        switch ($result.LogLevel) {
+            "Information" {  
+                Write-Information  -MessageData $result.Message
+                break
+            }
+            "Warning" {  
+                Write-Warning -Message $result.Message
+                break
+            }
+            "Error" {
+                if ($null -ne $result.exception) {
+                    if ($null -ne $result.exception.response) {
+                        try {
+                            $result.Exception.Response.GetResponseStream().position=0
+                            $responseMessage=$(New-object System.IO.StreamReader($result.Exception.Response.GetResponseStream())).ReadToEnd()
+                            Write-Error -Message "$($result.Message): $($responseMessage)"
+                        } catch {
+                            Write-Error -Message $result.Message -Exception $result.Exception
+                        }
+                    } else {
+                        Write-Error -Message $result.Message -Exception $result.Exception
+                    }
+                } else {
+                    Write-Error -Message $result.Message
+                }            
+                break
+            }
+            Default {
+                Write-Verbose -Message $result.Message -Verbose
+            }      
+        }
+        if (!$NoPassThru) {$result}
+    }
+    END {}
+}
+function Get-dplVariableDefinition() {
+    param(
+        $variableDefFile=".\variables.json",
+        $targetEnvironmentName
+    )
+    try {
+        $s=""
+        $variableDef=$(gc -Path $variableDefFile -Raw) | ConvertFrom-Json
+        $variables=$variableDef.Default
+        $envVariables=$variableDef.$($targetEnvironmentName)
+        $variables | get-member -MemberType NoteProperty | select -ExpandProperty name | %{
+            $variable=$_
+            if ($null -ne $variables.$($variable).schema) {
+                if ($null -ne $variables.$($variable).schema.$($targetEnvironmentName)) {
+                    $v=$variables.$($variable).schema.$($targetEnvironmentName)
+                    $variables.$($_) | Add-Member -MemberType NoteProperty -Name "value" -Value $($v) -Force
+                }
+            }
+            if ($null -ne $($envVariables.$($variable))) {
+                if ($null -ne $($envVariables.$($variable).value)) {
+                    $variables.$($variable) | Add-Member -MemberType NoteProperty -Name "value" -Value $($envVariables.$($_).value) -Force
+                }
+            }
+        }
+        for ($i=0;$i -lt 2;$i++) {
+            $variables | get-member -MemberType NoteProperty | select -ExpandProperty name | ? {$variables.$($_).value -like "*{*"} | %{
+                $variable=$_
+                $variables | Get-Member -MemberType NoteProperty | select -ExpandProperty name | %{
+                    $rVariable=$_
+                    if ($null -ne $variables.$($rVariable).value) {
+                        $variables.$($variable).value=$variables.$($variable).value.replace("{$($rVariable.tolower())}",$variables.$($rVariable).value)
+                    }
+                }
+                $envVariables | Get-Member -MemberType NoteProperty | select -ExpandProperty name | %{
+                    $rVariable=$_
+                    if ($null -ne $envVariables.$($rVariable).value) {
+                        $variables.$($variable).value=$variables.$($variable).value.replace("{$($rVariable.tolower())}",$envVariables.$($rVariable).value)
+                    }
+                }
+            }
+        }
+        $variables | get-member -MemberType NoteProperty | select -ExpandProperty name | %{
+            $variable=$_
+            if ($null -eq $variables.$($variable).value) {
+                Write-Warning -Message "No value specified for variable $($variable)"
+            } else {
+                if ($variables.$($variable).secure -eq $true) {
+                    $s+="@secure()`r`n"
+                }
+                $s+="@description('$($variables.$($variable).description)')`r`nparam $($variable) $($variables.$($variable).type) = $(if ($($variables.$($variable).type) -eq 'string') {"'"})$($variables.$($variable).value)$(if ($($variables.$($variable).type) -eq 'string') {"'"})`r`n`r`n"
+            }       
+        }
+        $obj=[PSCustomObject]@{
+            variables=$variables
+            variableString=$s
+        }
+        $r=New-Result -success $true -message "Successfully loaded variable definition ($($variableDefFile))" -value $obj -logLevel Information
+    } catch {
+        $r=New-Result -success $false -message "Error loading variable definition ($($variableDefFile))" -exception $_.Exception -logLevel Error
+    }
+    $r
+}
--- devops/iac/build.ps1
+++ devops/iac/build.ps1
@@ -1,2 +1,9 @@
+$targetEnvironmentName=$env:appEnv
+
+ipmo .\devops\helper.psm1
+
+
+$r=Get-dplVariableDefinition -targetEnvironmentName $targetEnvironmentName | Write-dplResult
+
 new-item .\deployment\iac -ItemType Directory
 "Test" | out-file .\deployment\iac\artifact_iac.txt
