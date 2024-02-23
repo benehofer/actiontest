32c92fd3c671b6651a5449b109969752aaddfbed
Hofer Benedikt <benedikt.hofer@wagner.ch>
2024-02-23 13:41:04 +0100

    113

--- devops/helper.psm1
+++ devops/helper.psm1
@@ -253,6 +253,7 @@ function Set-dplDirectoryDoc() {
         Set-dplDeploymentDirectory -deploymentDirectory $deploymentDirectory
         New-item -Path "$($deploymentDirectory)\doc" -ItemType Directory
         "<html><head><title>Test</title></head><body><h1>Test</h1></body></html>" | Out-File "$($deploymentDirectory)\doc\index.html" -Encoding utf8
+        "<html><head><title>Test 22</title></head><body><h1>Test</h1></body></html>" | Out-File "$($deploymentDirectory)\doc\general.html" -Encoding utf8
         $c=Get-Content -Path ".\doc\staticwebapp.config.source" -Encoding UTF8 -Raw
         $c=$c.Replace("###tenantid###",$variableDefinition.variables.tenant_id.value)
         $c | out-file "$($deploymentDirectory)\doc\staticwebapp.config.json" -Encoding utf8       
