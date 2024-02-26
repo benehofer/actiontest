6cc716b65a6cb2e078c72a48dfbc4af6378d4e99
Hofer Benedikt <benedikt.hofer@wagner.ch>
2024-02-26 19:40:55 +0100

    127

--- devops/tst/build.ps1
+++ devops/tst/build.ps1
@@ -5,9 +5,6 @@ $dpDir=$env:dpDir
 ipmo .\devops\helper.psm1 -force
 $InformationPreference="Continue"
 
-Write-Host $(git diff HEAD^ HEAD --name-only)
-Write-Host "end"
-
 $r=Get-dplVariableDefinition -targetEnvironmentName $appEnv | Write-dplResult
 if ($r.Success) {
     $variableDefinition=$r.Value
